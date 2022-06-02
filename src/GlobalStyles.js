e50b608e1d9bac1db38d347e2ff46a6eca508056
Julia Schneider <julia_schneider_mail@posteo.de>
2022-06-02 16:49:21 +0200

    add styled components

---
+++ src/GlobalStyles.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+  
+};
+
+body {
+  margin: 0;
+  padding:0;
+  font-family: 'Roboto', sans-serif;
+  font-size: 112.5%;
+  
+}
+
+h1, h2, h3, h4, h5, h6 {
+  padding: 0px;
+  margin: 0px;
+}
+
+input, label, textarea {
+  font-size: 1em
+}
+
+button {
+  font-size: 1em;
+  border: none;
+  background: transparent;
+  color: inherit;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+
+}`;
